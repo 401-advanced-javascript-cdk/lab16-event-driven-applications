823831b22a52a8c17838335f2a9f77efa76a9cc0
Chris Kozlowski <kozlowskicd@gmail.com>
2019-06-03 21:59:07 -0700

    refactor and add README.md

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 'use strict';
 
-// const names = require('./emitter-names.js');
-// const emitter = require('./emitter.js');
 require('./logger.js');
+const emitter = require('./emitter.js');
+const names = require('./emitter-names.js');
+const fs = require('fs');
+const {promisify} = require('util');
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+emitter.on(names.getArguments, args => {
+  let file = args.slice(2).shift();
+  emitter.emit(names.readFile, file);
+});
+
+emitter.on(names.readFile, async file => {
+  const data = await readFileAsync(file)
+  emitter.emit(names.upperCase, data, file); 
+});
+
+emitter.on(names.upperCase, (data, file) => {
+  let text = data.toString().toUpperCase();
+  emitter.emit(names.writeFile, text, file);
+});
+
+emitter.on(names.writeFile, async (text, file) => {
+  await writeFileAsync(file, Buffer.from(text));
+  console.log(`${file} saved`);
+})
+
+emitter.emit(names.getArguments, process.argv);
--- emitter.js
+++ emitter.js
@@ -2,33 +2,5 @@
 
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
-const names = require('./emitter-names.js');
-
-const fs = require('fs');
-const {promisify} = require('util');
-const readFileAsync = promisify(fs.readFile);
-const writeFileAsync = promisify(fs.writeFile);
-
-emitter.on(names.getArguments, args => {
-  let file = args.slice(2).shift();
-  emitter.emit(names.readFile, file);
-});
-
-emitter.on(names.readFile, async file => {
-  const data = await readFileAsync(file)
-  emitter.emit(names.upperCase, data, file); 
-});
-
-emitter.on(names.upperCase, (data, file) => {
-  let text = data.toString().toUpperCase();
-  emitter.emit(names.writeFile, text, file);
-});
-
-emitter.on(names.writeFile, async (text, file) => {
-  await writeFileAsync(file, Buffer.from(text));
-  console.log(`${file} saved`);
-})
-
-emitter.emit(names.getArguments, process.argv);
 
 module.exports = emitter;
